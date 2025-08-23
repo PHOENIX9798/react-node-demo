7d6382d9c7e4e53af44e5555d26db63f9a452528
PHOENIX9798 <792649438@qq.com>
2025-08-23 17:07:43 +0800

    feat: 工程化

--- xiu-demo/vite.config.ts
+++ xiu-demo/vite.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import viteEslint from 'vite-plugin-eslint';
+
+const envDir = path.resolve(process.cwd(), './env')
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,6 +19,29 @@ export default defineConfig({
         additionalData: '@import "./src/global.less";  ' // 或者自动将全局变量文件引入每个less文件中
       }
     }
-  }
+  },
+  envDir,
+  envPrefix: 'MFE_',
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx', '.scss', '.css'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // 源文件根目录
+      '@tests': path.resolve(__dirname, 'tests'), // 测试文件根目录
+      '@config': path.resolve(__dirname, 'config') // 配置文件根目录
+    }
+  },
+  server: {
+    open: true, // 自动打开浏览器
+    port: 3006, // 服务端口
+    proxy: {
+      '/api': '', // api代理路径
+      '/mock': '' // mock代理路径
+    }
+  },
+  plugins: [
+    react(),
+    viteEslint({
+      failOnError: false
+    })
+  ]
 });
-
